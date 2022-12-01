de4afc08c439825e0c799323bafdc0d06821b847
Jake Manley <jbmanley45@gmail.com>
2022-12-01 09:45:31 -0800

    commit tree eefc7b647f72f3f4ddd743df4d3bb2e4b3f4754e

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+    # In the index action, return JSON data representing a list of all students
+    def index 
+        students = Student.all 
+        render json: students
+    end
+    def grades 
+        students = Student.order(grade: :desc)
+        render json: students
+    end
+    def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+    end
+end
